3d43bcdf210fe2f0ddc07c0005ef9051499c5c7b
araf821 <araf.ahmed200@gmail.com>
2022-09-15 03:56:51 -0400

    Modified the css button property and removed unnecessary code

--- app.js
+++ app.js
@@ -2,7 +2,6 @@ const express = require("express");
 const bodyParser = require("body-parser");
 const mongoose = require("mongoose");
 const lodash = require("lodash");
-const functions = require(__dirname + "/functions.js");
 
 const app = express();
 
